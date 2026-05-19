9c75010b18a122e354a16eb9d7526de26df94a04
Shivam Soni <25ec3058@gmail.com>
2026-05-19 11:00:23 +0530

    fixed vercel deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Home from "./pages/Home";
-import Events from "./pages/Events";
-import Calendar from "./pages/Calendar";
-import Contact from "./pages/Contact";
-import EventDetails from "./pages/EventDetails";
-
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/events" element={<Events />} />
-        <Route path="/calendar" element={<Calendar />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/event/:id" element={<EventDetails />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    <div>
+      <h1>Website Working Successfully 🚀</h1>
+    </div>
+  )
 }
 
-export default App;
+export default App
